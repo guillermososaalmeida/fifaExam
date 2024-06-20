02d053155c310d27185a312339f75cb89cf6084a
BrisnaPaez <brisnapaez.13@gmail.com>
2024-06-20 20:44:38 +0200

    Create files and folders closes #18

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { DetailsComponent } from './pages/details/details.component';
+import { VideosComponent } from './pages/videos/videos.component';
+import { ButtonBackComponent } from './components/button-back/button-back.component';
+import { HomeCardComponent } from './components/home-card/home-card.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    DetailsComponent,
+    VideosComponent,
+    ButtonBackComponent,
+    HomeCardComponent,
+    NotFoundComponent,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/button-back/button-back.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-button-back',
+  templateUrl: './button-back.component.html',
+  styleUrl: './button-back.component.scss'
+})
+export class ButtonBackComponent {
+
+}
