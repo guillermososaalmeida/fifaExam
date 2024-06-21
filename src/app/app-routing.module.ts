28ea8aabf0715c5cc81cfbaa65aa98276aefa821
BrisnaPaez <brisnapaez.13@gmail.com>
2024-06-21 12:53:46 +0200

    Create routing breadcrumbs closes #12

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,34 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DetailsComponent } from './pages/details/details.component';
+import { VideosComponent } from './pages/videos/videos.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
+import { HomeComponent } from './pages/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  {
+    path: 'home', children: [
+      {
+        path: '',
+        component: HomeComponent
+      },
+      {
+        path: 'details', children: [
+          {
+            path: '',
+            component: DetailsComponent
+          },
+          {
+            path: 'videos',
+            component: VideosComponent
+          }
+        ]
+      },
+    ],
+  },
+  { path: '**', loadComponent: () => NotFoundComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
