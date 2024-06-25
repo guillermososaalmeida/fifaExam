25b42e6576dc87a5ba9ef2910d6ec8e39a6750e3
BrisnaPaez <brisnapaez.13@gmail.com>
2024-06-25 21:13:04 +0200

    Change services: encryption.services.ts

--- src/app/services/encryp-services/encryption.service.ts
+++ src/app/services/encryp-services/encryption.service.ts
@@ -1,44 +1,22 @@
 import * as CryptoJS from 'crypto-js';
-import { HttpClient } from '@angular/common/http';
-import { Injectable, inject } from '@angular/core';
-import { Observable, catchError, map, throwError } from 'rxjs';
-import { Player } from '../../classes/Player.class';
+import { Injectable } from '@angular/core';
 import { environment } from '../../../environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class EncryptionService {
-  private http = inject(HttpClient);
-  private apiUrl = environment.apiUrl;
   private password = environment.password;
 
-  decryptFile(): Observable<Player[]> {
-    const encryptedFilePath = this.apiUrl;
-
-    return this.http.get(encryptedFilePath, { responseType: 'text' }).pipe(
-      map((encryptedFileContent: string) => {
-        try {
-          const { encryptedData } = JSON.parse(encryptedFileContent);
-          const secretKey = this.password;
-
-          const bytes = CryptoJS.AES.decrypt(encryptedData, secretKey);
-          const decryptedJsonString = bytes.toString(CryptoJS.enc.Utf8);
-
-          const decryptedData = JSON.parse(decryptedJsonString);
-
-          return decryptedData;
-        } catch (error) {
-          console.error('Error al desencriptar:', error);
-          throw error;
-        }
-      }),
-      catchError((error) => {
-        console.error('Error al obtener el archivo encriptado:', error);
-        return throwError(
-          () => new Error('Error al obtener el archivo encriptado'),
-        );
-      }),
-    );
+  decryptContent(encryptedContent: string): string {
+    try {
+      const bytes = CryptoJS.AES.decrypt(encryptedContent, this.password);
+      const decryptedData = bytes.toString(CryptoJS.enc.Utf8);
+      console.log('decryptedData:', decryptedData);
+      return decryptedData;
+    } catch (error) {
+      console.error('Error:', error);
+      throw error;
+    }
   }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: '../assets/encryptedData.json',
+  apiUrl: 'U2FsdGVkX19zI6F9BwCgZg4j03Xv2zfDJ0iQDNgiC9OSn4fL0S2Jy2jU55yIHfax',
   password: 'micontraseñaultrasecreta',
 };
